b1352251ff7be958219357838906ea0cfeeaa79b
Nicolas Gonzalez <ngonzalez@snootlab.com>
2017-10-11 23:44:14 +0200

    Add three.js accelerometer view

---
+++ example4/static/js/mqtt_wrapper.js
@@ -0,0 +1,52 @@
+var mqtt;
+var reconnectTimeout = 2000;
+
+var host = "localhost";
+var port = 1882;
+var path = "/ws";
+var topic = "vector/";
+
+var useTLS = false;
+var username = null;
+var password = null;
+var cleansession = true;
+
+function MQTTconnect() {
+	mqtt = new Paho.MQTT.Client(
+			host,
+			port,
+			path,
+			"web_" + parseInt(Math.random() * 100, 10)
+	);
+
+	var options = {
+		timeout: 3,
+		useSSL: useTLS,
+		cleanSession: cleansession,
+		onSuccess: onConnect,
+		onFailure: onFailure,
+	};
+
+	mqtt.onConnectionLost = onConnectionLost;
+	mqtt.onMessageArrived = onMessageArrived;
+	mqtt.connect(options);
+}
+
+function onConnect() {
+	mqtt.subscribe(topic, {qos: 0});
+}
+
+function onConnectionLost(response) {
+	setTimeout(MQTTconnect, reconnectTimeout);
+};
+
+function onFailure(message) {
+	setTimeout(MQTTconnect, reconnectTimeout);
+}
+
+function onMessageArrived(message) {
+	var topic = message.destinationName;
+	var payload = JSON.parse(message.payloadString);
+
+	updateInterface(payload);
+};
